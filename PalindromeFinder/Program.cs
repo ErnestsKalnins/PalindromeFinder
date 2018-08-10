2fa123570782bd0b9cad16d7fd266d328a478619
Ernests Kalnins <ern.kalnins@gmail.com>
2018-08-10 08:25:36 +0300

    Initial commit

---
+++ PalindromeFinder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 9999; i >= 1000; i--)
+            {
+                for (int j = 9999; j >= 1000; j--)
+                {
+                    if (IsPalindrome(i * j))
+                    {
+                        Console.WriteLine("{0} * {1} = {2}", i, j, i * j);
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+
+        private static bool IsPalindrome(int num)
+        {
+            var numString = num.ToString();
+            var charArray = numString.ToCharArray();
+            Array.Reverse(charArray);
+            return numString == new string(charArray);
+        }
+    }
+}
